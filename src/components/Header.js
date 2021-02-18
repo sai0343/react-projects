60ce9138849831c70567e48b9e945f1e20c239fe
sai0343 <saeteja26@gmail.com>
2021-02-18 16:06:26 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,131 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import {
+  FormControl,
+  InputLabel,
+  Menu,
+  MenuItem,
+  MenuList,
+  Select,
+  Dialog,
+} from "@material-ui/core";
+import "../App.css";
+import RegisterForm from "./RegisterForm";
+import Popup from "./Popup";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    paddingTop: "5px",
+    paddingLeft: "20px",
+    display: "flex",
+    color: "white",
+    fontSize: "18px",
+  },
+  button: {
+    
+    marginLeft: "30em",
+    color: "white",
+    fontSize: "18px",
+    backgroundColor: "Orange",
+    display: "flex",
+  },
+}));
+
+// Header with Dropdown and register Buttons
+
+export default function Header() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const [age, setAge] = React.useState("");
+  const [openPopup, setOpenPopup] = React.useState(false);
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className="title">
+            News
+          </Typography>
+          <FormControl className={classes.formControl}>
+            <Select
+              value={age}
+              onChange={handleChange}
+              displayEmpty
+              className={classes.selectEmpty}
+            >
+              <MenuItem value="" disabled>
+                Placeholder
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <Select
+              value={age}
+              onChange={handleChange}
+              displayEmpty
+              className={classes.selectEmpty}
+            >
+              <MenuItem value="" disabled>
+                Placeholder
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <Select
+              value={age}
+              onChange={handleChange}
+              displayEmpty
+              className={classes.selectEmpty}
+            >
+              <MenuItem value="" disabled>
+                Placeholder
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <Select
+              value={age}
+              onChange={handleChange}
+              displayEmpty
+              className={classes.selectEmpty}
+            >
+              <MenuItem value="" disabled>
+                Placeholder
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+          <Popup className={classes.button} />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
